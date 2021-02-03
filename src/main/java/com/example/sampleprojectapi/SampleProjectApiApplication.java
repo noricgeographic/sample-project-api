d9b2b59cd792db3bc223bdc9dbaeacee8f9d10c5
noric/Yamada Takanori <77452880+noricgeographic@users.noreply.github.com>
2021-02-03 16:23:05 +0900

    初期コミット

---
+++ src/main/java/com/example/sampleprojectapi/SampleProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectApiApplication.class, args);
+	}
+
+}
